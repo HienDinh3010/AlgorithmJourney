08d65306ea3c98deaf45dd371d2fb18fea875827
HienDinh <dnmhien3010@gmail.com>
2023-09-04 21:56:20 -0500

    Valid Sudoku

---
+++ src/ArraysAndHashing/No36_ValidSudoku.java
@@ -0,0 +1,34 @@
+package ArraysAndHashing;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class No36_ValidSudoku {
+  public boolean isValidSudoku(char[][] board) {
+    for (int i = 0; i < 9; i++) {
+      Set<String> set = new HashSet<>();
+      for (int j = 0; j < 9; j++) {
+        if (board[i][j] != '.') {
+          String num = String.valueOf(board[i][j]);
+          if (set.contains(num)) {
+            return false;
+          }
+        }
+      }
+    }
+
+    for (int i = 0; i < 9; i++) {
+      Set<String> set = new HashSet<>();
+      for (int j = 0; j < 9; j++) {
+        if (board[j][i] != '.') {
+          String num = String.valueOf(board[i][j]);
+          if (set.contains(num)) {
+            return false;
+          }
+        }
+      }
+    }
+
+    return true;
+  }
+}
